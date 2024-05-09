d6928ec95f8184aa4e890fd8e0bb940ac7f06439
João Lucas Delistoianov Gonzalez <joao.gonzalez@sou.inteli.edu.br>
2024-05-09 09:15:06 -0300

    feat: metricas coletadas de acordo com requisição

--- ConsoleApp-metrics/ConsoleApp-metrics/Program.cs
+++ ConsoleApp-metrics/ConsoleApp-metrics/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using ConsoleApp_metrics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Diagnostics.Metrics;
 using System.Threading;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<HatCoMetrics>();
+
+var app = builder.Build();
+
+app.MapGet("/", (HatCoMetrics hatCoMetrics) =>
+{
+    hatCoMetrics.HatsSold(4);
+    return "Metric Recorded";
+});
+
+app.Run();
+
+public partial class Program
 {
     static Meter s_meter = new Meter("HatCo.Store");
     static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hatco.store.hats_sold");
 
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Projeto do João Executando");
         while (!Console.KeyAvailable)
